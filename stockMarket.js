945b973e776812738f0e4675fd9757f2c47c896c
ravindra rawat <ravinrawat.wd@gmail.com>
2018-10-26 07:50:15 -0600

    stock market

--- stockMarket.js
+++ stockMarket.js
@@ -2,11 +2,10 @@ const stockPrices = [45, 24, 35, 31, 40, 38, 11];
 
 let sellAt = 0;
 let sellAtIndex = 0;
-let buyAt = stockPrices[0];
-let profit = 0;
-let maxprofit = 0;
+let buyAt = stockPrices[0]; // first day of stock market
 
 stockPrices.forEach((price, index) => {
+  // highest stock price in a range  excluding the first day
   if (price > sellAt && index !== 0) {
     sellAt = price;
     sellAtIndex = index;
@@ -14,12 +13,13 @@ stockPrices.forEach((price, index) => {
   }
 });
 
+// find the minimum buyAt price , before the stock reached the peak price(index of sellAt)
 for (let i = 0; i < sellAtIndex; i++) {
   if (stockPrices[i] < buyAt) {
     buyAt = stockPrices[i];
   }
 }
 
-profit = sellAt - buyAt;
+let profit = sellAt - buyAt;
 
 console.log({ buyAt, sellAt, profit });
